1443c5b2d5e416a0c24c9257b82ddef1050c2b8a
Rafael Sánchez <34106668+rafasanchez14@users.noreply.github.com>
2026-02-13 21:52:40 +0100

    primer commit

---
+++ src/app/components/camera/camera.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camera',
+  imports: [],
+  templateUrl: './camera.html',
+  styleUrl: './camera.scss',
+})
+export class Camera {
+
+}
